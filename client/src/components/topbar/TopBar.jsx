1ca6c1ca2dc6235ddedaa9f134d76028ef704ab3
Dhanvi03 <pateldhanvi0310@gmail.com>
2024-02-09 21:11:28 +0530

    first commit

---
+++ client/src/components/topbar/TopBar.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+import "./topbar.css";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
+
+export default function TopBar() {
+  const {user, dispatch} = useContext(Context);
+  const PF = "http://localhost:5000/images/";
+
+
+  const handleLogout = ()=>{
+    dispatch({type : 'LOGOUT'})
+  }
+  return (
+    <div className='top'>
+      <div className="topLeft">
+        <img className="logo" src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png" alt="Blog"/>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link to='/' className="link"> Home</Link>
+          </li>
+          <li className="topListItem">
+            <Link to='/' className="link"> About</Link>
+          </li>
+          <li className="topListItem">
+            <Link to='/' className="link"> Contact</Link>
+          </li>
+          <li className="topListItem">
+            <Link to='/write' className="link"> write</Link>
+          </li>
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "Logout"}
+          </li>
+        </ul>
+      </div>
+      <div className="topRight">
+      {
+        user?(
+          <Link to='/settings'>
+          <img className="topImg" src={PF + user.profilePic} alt="pic"/>
+          </Link>
+            
+        ):(
+          <ul className="topList">
+          <li className="topListItem">
+            <Link to='/login' className="link">
+              LogIn
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link to='/register' className="link">
+              Register
+            </Link>
+          </li>
+          
+          
+          </ul>
+        )}
+     
+      <i className="topSearch fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  )
+}
